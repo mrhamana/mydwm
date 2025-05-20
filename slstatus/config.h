5c05c288c865d7ac46eeca4811709ab85ea84451
Saugat <saugatkhatiwada80@gmail.com>
2025-05-20 16:58:55 +0000

    modified the status bar

--- slstatus/config.h
+++ slstatus/config.h
@@ -56,6 +56,7 @@ static const char unknown_str[] = "n/a";
  *                                                     thermal zone on FreeBSD
  *                                                     (tz0, tz1, etc.)
  * uid                 UID of current user             NULL
+ * up                  interface is running            interface name (eth0)
  * uptime              system uptime                   NULL
  * username            username of current user        NULL
  * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
@@ -65,7 +66,5 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	
-	{ datetime, "%s",           "[%a %b %d-%m-%Y %r] " },
-	{ battery_perc,   "|| 󰁹: %s%%", "BAT0" },
+	{ datetime, "%s",           "%F %T" },
 };
