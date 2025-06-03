a59b98fe38f2c75b8fa8071f66724bbc6e55ee7b
mrhamana <saugatkhatiwada80@gmail.com>
2025-06-03 08:42:24 +0545

    added border and center patch to dwm

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,9 +2,10 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 0;               /* add an defined amount of pixels to the bar height */
-
 static const char *fonts[] = {
 	"monospace:size=10"
 };
@@ -16,10 +17,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      =10 ;
+static unsigned int lines      = 20;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
